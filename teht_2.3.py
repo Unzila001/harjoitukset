6e7b1b335f2995f8301a999963bc9abd1f55bc35
unzil <unzilasalman05@gmail.com>
2026-01-15 18:31:36 +0200

    kolmas tehtävä tehty

---
+++ teht_2.3.py
@@ -0,0 +1,5 @@
+kanta = float(input("Anna suorakulmion kanta: "))
+korkeus = float(input("Anna suorakulmion korkeus: "))
+piiri = (kanta * 2) + (korkeus * 2)
+ala = (kanta * korkeus)
+print(f"Suorakulmion piiri on {piiri:.2f} ja pinta-ala on {ala:.2f}")
