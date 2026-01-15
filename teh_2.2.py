6ce439024a2d569fb44cfbe55d49df3688dcfb5f
unzil <unzilasalman05@gmail.com>
2026-01-15 18:25:54 +0200

    toka tehtävä tehty

--- teh_2.2.py
+++ teh_2.2.py
@@ -3,4 +3,4 @@ sade = float(input("Anna ympyrän säteen: "))
 pi = 3.14
 #ympyrän pinta-ala on pi*r^2
 ala = pi * (sade ** 2)
-print(f"Ympyrän säteen ala on {ala}")
+print(f"Ympyrän säteen ala on {ala:.2f}")
