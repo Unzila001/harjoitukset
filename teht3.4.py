33c9993aef35a7d02bd22d434e5f832594c4eedf
unzil <unzilasalman05@gmail.com>
2026-01-18 00:03:25 +0200

    kolmas.neljäs tehtävä tehty

---
+++ teht3.4.py
@@ -0,0 +1,11 @@
+#Kysy käytäjältä vuosiluku
+vuosi = int(input("Anna vuosiluku: "))
+
+if vuosi % 400 == 0:
+    print("Vuosi on karkauvuosi.")
+elif vuosi % 100 == 0:
+    print("Vuosi ei ole karkauvuosi.")
+elif vuosi % 4 == 0:
+    print("Vuosi on karkauvuosi.")
+else:
+    print("Vuosi ei ole karkauvuosi.")
