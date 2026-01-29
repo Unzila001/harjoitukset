d7f6d94cf57eafc210c92a2760c1db1743e8cde3
unzil <unzilasalman05@gmail.com>
2026-01-29 14:43:23 +0200

    seit.toka tehtävä tehty

---
+++ teht_7.2.py
@@ -0,0 +1,15 @@
+nimet = set()
+while True:
+    nimi = input("Anna nim: ")
+
+    if nimi == "":
+        break
+
+    if nimi in nimet:
+        print("Aiemmin syötetty nimi")
+    else:
+        print("Uusi nimi")
+        nimet.add(nimi)
+print("\nSyötetyt nimet:")
+for n in nimet:
+    print(n)
