0ee4a686d833f35a20321e5f62a120e519663314
unzil <unzilasalman05@gmail.com>
2026-02-20 23:08:44 +0200

    kahdeksan.eka tehtävä tehty

--- teht_8.1.py
+++ teht_8.1.py
@@ -1,7 +1,7 @@
 import mysql.connector
 yhteys = mysql.connector.connect(
     host='127.0.0.1',
-    port=3308,
+    port=3307,
     database='flight_game',
     user='root',
     password='Unzila001',
@@ -13,13 +13,13 @@ kursori.execute("SELECT DATABASE()")
 print("DATABASE yhteys:", kursori.fetchone()[0])
 
 icao = input("Anna airport ICAO-koodi: ").upper()
-sql = "SELECT name, muncipality FROM airport WHERE ident = %s"
+sql = "SELECT name, municipality FROM airport WHERE ident = %s"
 kursori.execute(sql, (icao,))
 result = kursori.fetchone()
 
 if result:
     print(f"Lentokenettän nimi: {result[0]}")
-    print(f"Muncipality: {result[1]}")
+    print(f"Municipality: {result[1]}")
 
 else:
     print("Lentokenttää ei löydy")
