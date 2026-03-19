fbe3c31977b810a26a7862e34b37bc03cf93079f
Unzila001 <unzilasalman05@gmail.com>
2026-03-19 13:35:42 +0200

    yhdeksän_eka tehty

--- teht_9.1.py
+++ teht_9.1.py
@@ -1,1 +1,15 @@
-testi
+class Auto:
+
+    def __init__(self, rekisteritunnus, huippunopeus):
+        self.rekisteritunnus = rekisteritunnus
+        self.huippunopeus = huippunopeus
+        self.nopeus = 0
+        self.matka = 0
+
+#pääohjelma
+x = Auto("ABC-123", 142)
+
+print(x.rekisteritunnus)
+print(x.huippunopeus)
+print(x.nopeus)
+print(x.matka)
