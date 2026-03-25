eeb730bed0054ce20a1a52ef35d15a9c854aa6b5
Unzila001 <unzilasalman05@gmail.com>
2026-03-25 20:21:16 +0200

    kymppi_kolmas tehtävää tehty

---
+++ teht_10.3.py
@@ -0,0 +1,49 @@
+class Hissi:
+    def __init__(self, alaa, yllä):
+        self.yllä = yllä
+        self.alaa = alaa
+        self.nyt = alaa #aloita alimmasta kerroksesta
+
+    def kerros_ylös(self):
+        if self.nyt < self.yllä:
+            self.nyt += 1
+            print(f"Hissi on nyt {self.nyt} kerroksessa.")
+
+    def kerros_alas(self):
+        if self.nyt > self.alaa:
+            self.nyt -= 1
+            print(f"Hissi on nyt {self.nyt} kerroksessa.")
+
+    def siirry_kerrokseen(self, kerros):
+        while self.nyt < kerros:
+            self.kerros_ylös()
+        while self.nyt > kerros:
+            self.kerros_alas()
+
+class Talo:
+    def __init__(self, alaa, yllä, hissi_lkm):
+        self.yllä = yllä
+        self.alaa = alaa
+        self.hissit = []
+
+        for i in range(hissi_lkm):
+            hissi = Hissi(alaa, yllä)
+            self.hissit.append(hissi)
+
+    def aja_hissiä(self, hissi_num, kohde_kerros):
+        hissi = self.hissit[hissi_num - 1]
+        hissi.siirry_kerrokseen(kohde_kerros)
+
+    def palohälytys(self):
+        print("\nPalohälytys! Kaikki hissit pohjakerrokseen!\n")
+        for hissi in self.hissit:
+            hissi.siirry_kerrokseen(self.alaa)
+#pääohjelma
+
+talo = Talo(1,10,3)
+talo.aja_hissiä(1,5)
+talo.aja_hissiä(2,8)
+talo.aja_hissiä(1,1)
+
+talo.palohälytys()
+
