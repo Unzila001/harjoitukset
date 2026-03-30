f88b4c877bc150b25287fd682674dc991f544112
Unzila001 <unzilasalman05@gmail.com>
2026-03-30 20:00:32 +0300

    Eka Java tehtävää

---
+++ Java/koodi.js
@@ -0,0 +1,6 @@
+'use strict';
+
+for (let i = 10; i >= 0 ; i -= 2) {
+    console.log(i);
+}
+
---
+++ Java/teht_1.3.js
@@ -0,0 +1,14 @@
+'use strict'
+//Syötteiden luku
+let luku1 = +prompt("Anna eka luku");
+let luku2 = +prompt("Anna toka luku");
+let luku3 = +prompt("Anna kolmas luku");
+
+//Lasku
+let summa = luku1 + luku2 + luku3;
+let tulo = luku1 * luku2 * luku3;
+let ka = summa / 3;
+
+document.querySelector('#summa').innerHTML += summa;
+document.querySelector('#tulo').innerHTML += tulo;
+document.querySelector('#ka').innerHTML += ka;
